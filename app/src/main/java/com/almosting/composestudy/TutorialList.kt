8eb86fd227afa54594d1629ea3396ebe2c7fe667
w.feng <w.feng@sunmedia.com.cn>
2023-01-18 16:42:02 +0800

    update button textfield image compose

--- app/src/main/java/com/almosting/composestudy/TutorialList.kt
+++ app/src/main/java/com/almosting/composestudy/TutorialList.kt
@@ -9,6 +9,9 @@ import androidx.compose.ui.res.stringResource
 import com.almosting.composestudy.chapter1.Tutorial1_1Screen
 import com.almosting.composestudy.chapter1.Tutorial1_2Screen
 import com.almosting.composestudy.chapter2.Tutorial2_1Screen
+import com.almosting.composestudy.chapter2.Tutorial2_2Screen
+import com.almosting.composestudy.chapter2.Tutorial2_3Screen
+import com.almosting.composestudy.chapter2.Tutorial2_4Screen
 import com.almosting.composestudy.model.TutorialSectionModel
 
 /**
@@ -76,10 +79,63 @@ fun createComponentTutorialList(onBack: () -> Unit): List<TutorialSectionModel> 
         )
     )
 
+    val tutorial2_2 = TutorialSectionModel(
+        title = stringResource(R.string.title2_2),
+        description = "Create Button with text and/or with image, Floating Action Button " +
+                ", or Chips. Modify properties of buttons such as color, text, or state.",
+        action = {
+            Tutorial2_2Screen()
+        },
+        tags = listOf(
+            TAG_COMPOSE,
+            TAG_BUTTON,
+            TAG_ICON_BUTTON,
+            TAG_FAB_BUTTON,
+            TAG_CHIP
+        )
+    )
+
+    val tutorial2_3 = TutorialSectionModel(
+        title = stringResource(R.string.title2_3),
+        description = "Create TextField component with regular style or outlined. Set error," +
+                " colors, state, icons, VisualTransformations for phone or " +
+                "credit card, and IME actions.",
+        action = {
+            Tutorial2_3Screen()
+        },
+        tags = listOf(
+            TAG_COMPOSE,
+            TAG_TEXT_FIELD,
+            TAG_OUTLINED_TEXT_FIELD,
+            TAG_IME,
+            TAG_VISUAL_TRANSFORMATION,
+            TAG_REGEX
+        )
+    )
+
+    val tutorial2_4 = TutorialSectionModel(
+        title = stringResource(R.string.title2_4),
+        description = "Create Image to display images, set image and crop styles. " +
+                "Change shape of Image or apply ColorFilter and PorterDuff modes.",
+        action = {
+            Tutorial2_4Screen()
+        },
+        tags = listOf(
+            TAG_COMPOSE,
+            TAG_IMAGE,
+            TAG_DRAWABLE,
+            TAG_VECTOR_DRAWABLE,
+            TAG_BITMAP
+        )
+    )
+
     return listOf(
         tutorial1_1,
         tutorial1_2,
-        tutorial2_1
+        tutorial2_1,
+        tutorial2_2,
+        tutorial2_3,
+        tutorial2_4
     )
 }
 
